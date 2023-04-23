3cda8f05820a3226757d07855c387eda49b2fda5
kirilyotov <kirilyotov@outlok.com>
2023-04-23 23:08:29 +0300

    cmd class created

---
+++ src/CommandLine/cmd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by KirilYotov on 23.4.2023 г..
+//
+
+#include "cmd.h"
---
+++ src/CommandLine/cmd.h
@@ -0,0 +1,10 @@
+#ifndef LIBRARY_CMD_H
+#define LIBRARY_CMD_H
+
+
+class cmd {
+
+};
+
+
+#endif //LIBRARY_CMD_H

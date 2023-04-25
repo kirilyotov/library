8d1e526aa0678998c5cb12f3f86255134d5f3f5c
kirilyotov <kirilyotov@outlok.com>
2023-04-25 23:29:18 +0300

    Util class created

---
+++ cmd/Utils.cpp
@@ -0,0 +1,7 @@
+#include "Utils.h"
+
+const std::string Utils::CLOSE = "close";
+const std::string Utils::SAVE = "save";
+const std::string Utils::SAVE_AS = "save as";
+const std::string Utils::HElP = "help";
+const std::string Utils::EXIT = "exit";
---
+++ cmd/Utils.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARY_UTILS_H
+#define LIBRARY_UTILS_H
+
+#include <iostream>
+#include <string>
+
+class Utils {
+    static const std::string CLOSE;
+    static const std::string SAVE;
+    static const std::string SAVE_AS;
+    static const std::string HElP;
+    static const std::string EXIT;
+};
+
+
+#endif //LIBRARY_UTILS_H

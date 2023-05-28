fa491ea5b85b510ff3c935ee7594d55a71d3b418
kirilyotov <kirilyotov@outlok.com>
2023-05-28 22:09:34 +0300

    Read and write to file

--- Digital-Library/src/IOStream/IOStream.cpp
+++ Digital-Library/src/IOStream/IOStream.cpp
@@ -1,5 +1,32 @@
-//
-// Created by KirilYotov on 28.5.2023 г..
-//
-
 #include "IOStream.h"
+
+void IOStream::writeAccounts(const AdminPanel& adminPanel) {
+    std::ofstream file ("./passwords.txt");
+
+    if(!file.is_open()){
+        throw std::runtime_error("Problem while opening file!");
+    }
+    file << adminPanel;
+    file.close();
+}
+
+void IOStream::readBooks(const std::string &path, Library& library) {
+    std::fstream file(path);
+    if(!file.is_open()){
+        throw std::runtime_error("Problem while writing to file!");
+    }
+    while (!file.eof()){
+        file >> library;
+
+    }
+}
+
+void IOStream::writeBooks(const std::string &path, const Library& library) {
+    std::ofstream file (path);
+
+    if(!file.is_open()){
+        throw std::runtime_error("Problem while opening file!");
+    }
+    file << library;
+    file.close();
+}
--- Digital-Library/src/IOStream/IOStream.h
+++ Digital-Library/src/IOStream/IOStream.h
@@ -1,9 +1,18 @@
 #ifndef LIBRARY_IOSTREAM_H
 #define LIBRARY_IOSTREAM_H
-
-
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "../AdminPanel/AdminPanel.h"
+#include "../Library/Library.h"
+#include "../Book/Book.h"
 class IOStream {
+public:
+    static void writeAccounts(const AdminPanel& adminPanel);
 
+    static void readBooks(const std::string& path, Library& library);
+
+    static void writeBooks(const std::string& path, const Library& library);
 };
 
 
